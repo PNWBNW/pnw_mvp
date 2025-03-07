15b83658877c4327b7bf80be4a3d964697e42058
Joshua Day <95725098+PNWBNW@users.noreply.github.com>
2025-03-06 23:28:49 -0800

    Update deploy_pnw_mvp.sh

--- deploy_pnw_mvp.sh
+++ deploy_pnw_mvp.sh
@@ -23,12 +23,12 @@ programs=(
 
 echo "🔥 Starting deployment funnel for PNW-MVP..."
 
-cd src  # Move into the source directory
-
 for contract in "${programs[@]}"
 do
   echo "🟢 Deploying $contract..."
-  leo deploy --network testnet --private-key ${ALEO_PRIVATE_KEY} "$contract"
+  cd src/$contract
+  leo deploy --network testnet --private-key ${ALEO_PRIVATE_KEY}
+  cd - > /dev/null  # Move back to the main directory silently
   if [ $? -eq 0 ]; then
       echo "✅ Successfully deployed $contract"
   else
@@ -36,7 +36,5 @@ do
       exit 1
   fi
 done
-
-cd ..  # Move back to the root directory
 
 echo "🎉 All contracts deployed successfully!"
